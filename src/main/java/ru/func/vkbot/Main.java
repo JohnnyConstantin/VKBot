6c3758d5699988ddabd4bf812544ee199575a02b
Krasav4ik <zkonst12@gmail.com>
2020-04-20 05:04:40 +0300

    need to fix music and weather

--- src/main/java/ru/func/vkbot/Main.java
+++ src/main/java/ru/func/vkbot/Main.java
@@ -1,21 +1,17 @@
 package ru.func.vkbot;
 
-import com.petersamokhin.bots.sdk.callbacks.Callback;
-import com.petersamokhin.bots.sdk.callbacks.commands.OnCommandCallback;
-import com.petersamokhin.bots.sdk.clients.User;
-import com.petersamokhin.bots.sdk.longpoll.LongPoll;
 import com.petersamokhin.bots.sdk.objects.Message;
 import com.petersamokhin.bots.sdk.clients.Group;
-import com.petersamokhin.bots.sdk.clients.Client;
-import com.petersamokhin.bots.sdk.utils.vkapi.API;
-import org.json.JSONObject;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 
 public class Main {
     private static final String KEY = "3d073a95976f69f8a074679ece478de1a1f3aec5d75fdcd2a7c5e13f2d2be735fa50e592c01aa3b905c4f";
     private static final String MyName = "VKbot";
+    private static String url = "https://tts.voicetech.yandex.net/generate?format=mp3&lang=ru&speaker=zahar&key=\" + yandexKey + \"&text=";;
+    String yandexKey = "key";
     public static void main(String[] args) {
         Group group = new Group(194437959, KEY);
         /*group.onTyping(userId -> {
@@ -33,6 +29,8 @@ public class Main {
                     .text("Поразительно красивый стикер! " + MyName + " одобряет");
             sticker.send();
         });
+
+
 
         group.onCommand("!help", message -> {
             Message help = new Message()
@@ -73,13 +71,12 @@ public class Main {
             hz.send();
         });
 
-        group.onCommand("!music", message -> {
-            Message hz = new Message()
+       /* group.onMessage(message -> {
+            new Message()
                     .from(group)
                     .to(message.authorId())
-                    .text("Если здесь нет рандомного аудио, значит что-то пошло не так");
-            hz.send();
-        });
+                    .sendVoiceMessage(url + URLEncoder.encode(message.getText()));
+        });*/
 
         group.onVoiceMessage(message -> {
             Message voice = new Message()
