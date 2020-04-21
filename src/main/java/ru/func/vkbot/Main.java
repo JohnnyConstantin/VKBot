0214b597c783e45af5e32fc255300bad5166de53
Krasav4ik <zkonst12@gmail.com>
2020-04-21 03:20:12 +0300

    Returned to handmade API

--- src/main/java/ru/func/vkbot/Main.java
+++ src/main/java/ru/func/vkbot/Main.java
@@ -1,72 +1,53 @@
 package ru.func.vkbot;
 
 import com.petersamokhin.bots.sdk.clients.Group;
-import com.petersamokhin.bots.sdk.longpoll.LongPoll;
 import com.petersamokhin.bots.sdk.objects.Message;
-
-import java.util.Random;
 
 
 public class Main {
     private static final String KEY = "3d073a95976f69f8a074679ece478de1a1f3aec5d75fdcd2a7c5e13f2d2be735fa50e592c01aa3b905c4f";
     private static final String MyName = "VKbot";
-    private static String url = "https://tts.voicetech.yandex.net/generate?format=mp3&lang=ru&speaker=zahar&key=\" + yandexKey + \"&text=";;
-    String yandexKey = "key";
 
     public static void main(String[] args) throws Exception {
 
         Group group = new Group(194437959, KEY);
 
-
         group.onCommand("!weather", message -> {
-            Weather weather = new Weather();
-            System.out.println("Testing 1 - Send Http GET request");
-            String result = null;
-            try {
-                result = weather.res(weather.sendGet());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            Message wether = new Message()
-                    .from(group)
-                    .to(message.authorId())
-                    .text(result);
-            wether.send();
-        });
+            String[] arg = message.getText().split(" ");
+            String command = arg[1];
+                        Weather weather = new Weather();
+                         System.out.println("Testing 1 - Send Http GET request");
+                         String result = "Города " + command + "не существует";
+                             try {
+                                     result = weather.res(weather.sendGet(command));
+                                 } catch (Exception e) {
+                                     e.printStackTrace();
+                                 }
+                             Message wether = new Message()
+                             .from(group)
+                             .to(message.authorId())
+                             .text(result);
+                             wether.send();
+            });
 
         group.onCommand("!help", message -> {
             Message help = new Message()
                     .from(group)
                     .to(message.authorId())
                     .text("Доступные команды:\n" +
-                            "!help\t Узнать доступные команды\n" +
-                            "!weatherCity\t Установить текущий город(в разработке)\n" +
-                            "!weather\t Узнать погоду в текущем городе(Москва по умолчанию)\n" +
+                            "!help\t Узнать доступные команды\n"+
+                            "!weather NAME_OF_CITY\t Узнать погоду в текущем городе\n" +
                             "!loh\t Узнать лох Вы или нет\n"
                             );
-            LongPoll poll = new LongPoll(group);
-
             help.send();
         });
 
         group.onCommand("!loh", message -> {
-            Random rand = new Random();
-
             Message loh = new Message()
                     .from(group)
                     .to(message.authorId())
                     .text("Ого! Да Вы, кажется, лох");
             loh.send();
-        });
-
-
-
-        group.onCommand("!weatherCity", message -> {
-
-            Message weatherCity = new Message()
-                    .from(group)
-                    .to(message.authorId());
-            weatherCity.send();
         });
 
         group.onStickerMessage( message -> {
--- src/main/java/ru/func/vkbot/Weather.java
+++ src/main/java/ru/func/vkbot/Weather.java
@@ -11,9 +11,9 @@ public class Weather {
     private final String USER_AGENT = "Mozilla/5.0";
 
     // HTTP GET request
-    protected String sendGet() throws Exception {
+    protected String sendGet(String city) throws Exception {
 
-        String url = "https://api.openweathermap.org/data/2.5/weather?q=Moscow,ru&appid=" + ApiKey.getApiKey();
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + ApiKey.getApiKey();
 
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -25,6 +25,7 @@ public class Weather {
         con.setRequestProperty("User-Agent", USER_AGENT);
 
         int responseCode = con.getResponseCode();
+
         System.out.println("\nSending 'GET' request to URL : " + url);
         System.out.println("Response Code : " + responseCode);
 
@@ -47,21 +48,21 @@ public class Weather {
     protected String res(String response) {
 
         JSONObject jsonObject = new JSONObject(response.toString());
-        String city = jsonObject.getString("name");
-        double temp = Math.round(jsonObject.getJSONObject("main").getDouble("temp") - 273.15);
-        double humidity = jsonObject.getJSONObject("main").getDouble("humidity");
-        double wind = jsonObject.getJSONObject("wind").getDouble("speed");
-        String overcast = jsonObject.getJSONArray("weather").getJSONObject(0).getString("description");
-
-        return  "City: "
-                + city + "\n"
-                + "Current temperature: "
-                + temp + "\n"
-                + "Current humidity: "
-                + humidity + "\n"
-                + "Wind speed: "
-                + wind + "\n"
-                + "Description: "
-                + overcast;
+            String city = jsonObject.getString("name");
+            double temp = Math.round(jsonObject.getJSONObject("main").getDouble("temp") - 273.15);
+            double humidity = jsonObject.getJSONObject("main").getDouble("humidity");
+            double wind = jsonObject.getJSONObject("wind").getDouble("speed");
+            String overcast = jsonObject.getJSONArray("weather").getJSONObject(0).getString("description");
+            return "City: "
+                    + city + "\n"
+                    + "Current temperature: "
+                    + temp + "\n"
+                    + "Current humidity: "
+                    + humidity + "\n"
+                    + "Wind speed: "
+                    + wind + "\n"
+                    + "Description: "
+                    + overcast;
     }
 }
+
